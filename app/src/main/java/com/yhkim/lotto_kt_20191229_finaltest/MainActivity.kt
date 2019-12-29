e81a959d7de0cdb826bc655cd4de021800ae7486
kvoyant <kvoyant@gmail.com>
2019-12-29 10:15:58 +0900

    Initial commit

---
+++ app/src/main/java/com/yhkim/lotto_kt_20191229_finaltest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.yhkim.lotto_kt_20191229_finaltest
+
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+}
