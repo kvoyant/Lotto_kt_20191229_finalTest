04469de293dbbeda6267aa03aba8c24dee28ed71
kvoyant <kvoyant@gmail.com>
2019-12-29 10:51:09 +0900

    6개의 당첨번호 생성 초안  - 테스트 미실시

--- app/src/main/java/com/yhkim/lotto_kt_20191229_finaltest/LottoActivity.kt
+++ app/src/main/java/com/yhkim/lotto_kt_20191229_finaltest/LottoActivity.kt
@@ -1,9 +1,13 @@
 package com.yhkim.lotto_kt_20191229_finaltest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_lotto.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class LottoActivity : BaseActivity() {
+
+    val winLottoNumArr = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +16,40 @@ class LottoActivity : BaseActivity() {
         setValues()
     }
     override fun setupEvents() {
+        buyOneLottoBtn.setOnClickListener {
+//            6개의 숫자를 랜덤으로 생성 => 텍스트뷰 6개에 반영
+            makeWinLottoNum()
+        }
+    }
+
+    fun makeWinLottoNum() {
+//        6개의 당첨 번호 생성 => 6번 반복
+//        랜덤으로 숫자를 생성 => 제약조건 : 1 ~ 45 사이 , 중복허용안함
+//        제약조건을 통과한다면 => 당첨번호 목록으로 추가 : 배열 사용(ArrayList)
+//        작은 숫자 부터 정렬
+//        여기까지 완료되면 6개 텍스트뷰에 반영 예정
+
+        for(i in 0..5) {
+
+            while (true) {
+                val randomInt = Random().nextInt(45) + 1 // 0 ~ 44의 랜덤값 + 1 => 1 ~ 45의 랜덤값
+
+                var isDupOk = true // 중복검사여부
+                for(num in winLottoNumArr) {
+                    if(randomInt == num) {
+//                    중복 확인
+                        isDupOk = false
+                        break
+                    }
+                }
+
+                if(isDupOk) {
+                    winLottoNumArr.add(randomInt)
+                    break
+                }
+            }
+
+        }
     }
 
     override fun setValues() {
--- app/src/main/java/com/yhkim/lotto_kt_20191229_finaltest/MainActivity.kt
+++ app/src/main/java/com/yhkim/lotto_kt_20191229_finaltest/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.yhkim.lotto_kt_20191229_finaltest
 
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,11 @@ class MainActivity : BaseActivity() {
         setValues()
     }
     override fun setupEvents() {
+        lottoBtn.setOnClickListener {
+            val intent = Intent(mContext, LottoActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     override fun setValues() {
